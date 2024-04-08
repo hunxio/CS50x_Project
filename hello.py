ba88901d7e83a72a218675bc8b2b2c540a5f8203
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-08 18:32:53 +0200

    Layout update

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello world!'
+    return render_template("layout.html")
