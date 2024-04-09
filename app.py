855a0527599c6f3bc82ca1d11031f08ed66e1d10
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-09 19:11:59 +0200

    Started on signup page, renamed it from the previous register.html. Added connection through app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
-@app.route('/')
 
+@app.route("/")
 def home():
     return render_template("homepage.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        return redirect("homepage.html")
+    return render_template("signup.html")
