6d11d24b7dffae3412e4dc5a0db8ede8f037cd42
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-10 18:10:39 +0200

    added error page access, added some error checks while trying to signing up an account (not finished yet)

--- app.py
+++ app.py
@@ -1,23 +1,45 @@
 import os
+
 import sqlite3
 from flask import Flask, render_template, redirect, request
+import re
 
 app = Flask(__name__)
+
 con = sqlite3.connect("database.db")
 cur = con.cursor()
+
 @app.route("/")
 def home():
     return render_template("homepage.html")
 
-@app.route("/signup", methods=["GET", "POST"])
+@app.route("/errorpage")
+def errorpage():
+    return render_template("errorpage.html")
+
+@app.route("/signup", methods=["GET","POST"])
 def signup():
     if request.method == "POST":
-        firstName = request.form.get("name")
+        name = request.form.get("name")
+        if not name:
+            return redirect("/errorpage")
         lastName = request.form.get("lastname")
+        if not lastName:
+            return redirect("/errorpage")
         email = request.form.get("email")
+        if not email:
+            return redirect("/errorpage")
         username = request.form.get("username")
+        if not username:
+            return redirect("/errorpage")
         password = request.form.get("password")
-        return redirect("/")
+        passwordValidation = re.search(r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}", password)
+        if not password:
+            return redirect("/errorpage")
+        elif len(password) < 8 or len(password) > 16:
+            return redirect("/errorpage")
+        elif passwordValidation is None:
+            return redirect("/errorpage")
     return render_template("signup.html")
 
 @app.route("/login", methods=["GET", "POST"])
