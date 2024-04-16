b7c2a238e40811fbf68e3375701c6a6f3c4e272c
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-16 14:46:05 +0200

    Changed name defs

--- personaldefs.py
+++ personaldefs.py
@@ -2,6 +2,9 @@ import os
 import sqlite3
 from flask import Flask, render_template, redirect, request
 
-def returnErrorMessage(con, page, code_error):
+def ErrorConnection(con, page, code_error):
     con.close()
+    return render_template(page, message=code_error)
+
+def ErrorTemplate(page, code_error):
     return render_template(page, message=code_error)
