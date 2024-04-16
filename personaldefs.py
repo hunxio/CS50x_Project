ff81c5c4f1900a8b54cd91aff69869ea1b3b89a4
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-16 14:48:31 +0200

    Comments on def roles

--- personaldefs.py
+++ personaldefs.py
@@ -2,9 +2,11 @@ import os
 import sqlite3
 from flask import Flask, render_template, redirect, request
 
+# It will close the database connection and return an error page #
 def ErrorConnection(con, page, code_error):
     con.close()
     return render_template(page, message=code_error)
 
+# Basic template for error page, in case no access to the database and user did not login #
 def ErrorTemplate(page, code_error):
     return render_template(page, message=code_error)
