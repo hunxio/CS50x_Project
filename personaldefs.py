f5cef5012c3a99148fce89ed46780dfed0b25c73
Huno <136697789+hunxio@users.noreply.github.com>
2024-04-22 19:00:58 +0200

    new def for acquiring the user's username when a session in live

--- personaldefs.py
+++ personaldefs.py
@@ -1,6 +1,6 @@
 import os
 import sqlite3
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, session
 
 # It will close the database connection and return an error page #
 def ErrorConnection(con, code_error):
@@ -10,3 +10,9 @@ def ErrorConnection(con, code_error):
 # Basic template for error page, in case no access to the database and user did not login #
 def ErrorTemplate(code_error):
     return render_template("errorpage.html", message=code_error)
+
+def acquireSessionEmail(cur):
+    email = session.get("email")
+    usernameCur = cur.execute("SELECT username FROM users WHERE email = ?;", (email,))
+    userUsername = usernameCur.fetchone()[0]
+    return userUsername
